822d221a96cd736a10c4e1d3c79ebc02168b54e8
Akshay Pande <akspande1989@gmail.com>
2021-06-12 17:42:15 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,11 +9,12 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class UsersController : BaseApiController
     {
         
@@ -44,5 +45,22 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username); 
 
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+
+        }
+
     }
 }
---
+++ API/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace API.DTOs
+{
+	public class MemberUpdateDto
+	{
+		public string Introduction { get; set; }
+		public string LookingFor { get; set; }
+		public string Interests { get; set; }
+		public string City { get; set; }
+		public string Country { get; set;  }
+
+	}
+}
